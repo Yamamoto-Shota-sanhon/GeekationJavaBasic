8bc4a886a8afd684476f7286375f9245d8fbbb11
Yamamoto-Shota-sanhon <s.yamamoto.cingroup2012@gmail.com>
2025-01-29 21:58:25 +0900

    私の好きな数字の実装

---
+++ src/basicSyntax/Variables.java
@@ -0,0 +1,21 @@
+package basicSyntax;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+		int num1 = 4;
+		double num2 = 1.5;
+		
+	    String iLike = "私の好きな";
+	    String desu = "です。";
+	    
+
+	    System.out.println(num1);
+	    System.out.println(num2);
+	    System.out.println(iLike + "整数は" + num1 + desu);
+	    System.out.println(iLike + "小数は" + num2 + desu);
+	}
+
+}
