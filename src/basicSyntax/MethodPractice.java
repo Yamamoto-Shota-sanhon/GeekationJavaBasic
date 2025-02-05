a039ecc2812dcd431b1ac1eaf942d00ddb42cc5a
Yamamoto-Shota-sanhon <s.yamamoto.cingroup2012@gmail.com>
2025-02-05 09:28:11 +0900

    メソッドの修正

--- src/basicSyntax/MethodPractice.java
+++ src/basicSyntax/MethodPractice.java
@@ -2,54 +2,62 @@ package basicSyntax;
 
 public class MethodPractice {
 
-	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-		int a = 44;
-		int b = 4;
+    public static void main(String[] args) {
+        // TODO 自動生成されたメソッド・スタブ
+        int a = 44;
+        int b = 4;
+        int[] numbers = { a, b };
 
-		output(a, b);
+        output(numbers);
 
-	}
-//足し算
-	public static int add(int a, int b) {
-		int sum = a + b;
+    }
 
-		return sum;
-	}
-//引き算
-	public static int sub(int a, int b) {
-		int difference = a - b;
+    //足し算
+    public static int add(int a, int b) {
+        int sum = a + b;
 
-		return difference;
-	}
-//掛け算
-	public static int mult(int a, int b) {
-		int product = a * b;
+        return sum;
+    }
 
-		return product;
-	}
-//割り算
-	public static int div(int a, int b) {
-		int quotient = a / b;
+    //引き算
+    public static int sub(int a, int b) {
+        int difference = a - b;
 
-		return quotient;
-	}
-//出力
-	public static void output(int a, int b) {
-		String answer = "計算結果は";
+        return difference;
+    }
 
-		int sum = MethodPractice.add(a, b);
-		System.out.println(answer + sum + "です。");
+    //掛け算
+    public static int mult(int a, int b) {
+        int product = a * b;
 
-		int difference = MethodPractice.sub(a, b);
-		System.out.println(answer + difference + "です。");
+        return product;
+    }
 
-		int product = MethodPractice.mult(a, b);
-		System.out.println(answer + product + "です。");
+    //割り算
+    public static int div(int a, int b) {
+        int quotient = a / b;
 
-		int quotient = MethodPractice.div(a, b);
-		System.out.println(answer + quotient + "です。");
+        return quotient;
+    }
 
-	}
+    //出力
+    public static void output(int[] numbers) {
+        String answer = "計算結果は";
+        int a = numbers[0];
+        int b = numbers[1];
+
+        int sum = MethodPractice.add(a, b);
+        System.out.println(answer + sum + "です。");
+
+        int difference = MethodPractice.sub(a, b);
+        System.out.println(answer + difference + "です。");
+
+        int product = MethodPractice.mult(a, b);
+        System.out.println(answer + product + "です。");
+
+        int quotient = MethodPractice.div(a, b);
+        System.out.println(answer + quotient + "です。");
+
+    }
 
 }
