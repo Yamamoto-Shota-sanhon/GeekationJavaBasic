8f53ec012b6bb8d0c49b1f9407ec82d547fea65a
Yamamoto-Shota-sanhon <s.yamamoto.cingroup2012@gmail.com>
2025-02-08 17:35:15 +0900

    クラスの実装

---
+++ src/basicSyntax/Main.java
@@ -0,0 +1,19 @@
+package basicSyntax;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // TODO 自動生成されたメソッド・スタブ
+        int age = 10;
+        double height = 202.3;
+        
+        int num1 = 150;
+        int num2 = 320;
+        
+        GeekIntroduction.greeting(age, height);
+        
+        GeekIntroduction.specoalSkill(num1, num2);
+
+    }
+
+}
