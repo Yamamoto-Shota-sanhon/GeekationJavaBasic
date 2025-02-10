d624b2487cbdadef4c72788ce5627808adda2fa4
Yamamoto-Shota-sanhon <s.yamamoto.cingroup2012@gmail.com>
2025-02-10 16:50:17 +0900

    クラスの修正

--- src/basicSyntax/GeekIntroduction.java
+++ src/basicSyntax/GeekIntroduction.java
@@ -13,17 +13,17 @@ public class GeekIntroduction {
 
     public static void specoalSkill(int num1, int num2) {
 
-        if (num1 == 0 || num2 <= 0) {
-            System.out.println("num1とnum2は0より大きい値にしてください。");
-        } else if (num1 >= num2) {
-            System.out.println("num2はnum1より大きい値にしてください。");
-        } else if (num1 <= 300 || num2 <= 300) {
-            System.out.println("num1とnum2は300以下にしてください");
-        } else {
-
-        }
         for (int num = 1; num <= num2; num++) {
-            if (num % 3 == 0 && num % 5 == 0) {
+            if (num1 == 0 || num2 <= 0) {
+                System.out.println("num1とnum2は0より大きい値にしてください。");
+                break;
+            } else if (num1 >= num2) {
+                System.out.println("num2はnum1より大きい値にしてください。");
+                break;
+            } else if (num1 > 300 || num2 > 300) {
+                System.out.println("num1とnum2は300以下にしてください");
+                break;
+            } else if (num % 3 == 0 && num % 5 == 0) {
                 System.out.println(num + "は3の倍数かつ5の倍数です。");
                 continue;
             } else if (num % 3 == 0) {
@@ -32,10 +32,13 @@ public class GeekIntroduction {
             } else if (num % 5 == 0) {
                 System.out.println(num + "は5の倍数です。");
                 continue;
+            } else if (num <= num2) {
+                System.out.println(num);
+            } else {
+                break;
             }
 
         }
 
     }
-
 }
--- src/basicSyntax/Main.java
+++ src/basicSyntax/Main.java
@@ -6,12 +6,12 @@ public class Main {
         // TODO 自動生成されたメソッド・スタブ
         int age = 10;
         double height = 202.3;
-        
+
         int num1 = 150;
         int num2 = 320;
-        
+
         GeekIntroduction.greeting(age, height);
-        
+
         GeekIntroduction.specoalSkill(num1, num2);
 
     }
