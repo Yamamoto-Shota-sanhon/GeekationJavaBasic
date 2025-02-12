5b712b33c96797bcd89e079ea3b5a94277394def
Yamamoto-Shota-sanhon <s.yamamoto.cingroup2012@gmail.com>
2025-02-12 13:59:37 +0900

    クラスの修正3

--- src/basicSyntax/GeekIntroduction.java
+++ src/basicSyntax/GeekIntroduction.java
@@ -13,17 +13,18 @@ public class GeekIntroduction {
 
     public static void specoalSkill(int num1, int num2) {
 
-        for (int num = 1; num <= num2; num++) {
-            if (num1 <= 0 || num2 <= 0) {
-                System.out.println("num1とnum2は0より大きい値にしてください。");
-                break;
-            } else if (num1 >= num2) {
-                System.out.println("num2はnum1より大きい値にしてください。");
-                break;
-            } else if (num1 > 300 || num2 > 300) {
-                System.out.println("num1とnum2は300以下にしてください");
-                break;
-            } else if (num % 3 == 0 && num % 5 == 0) {
+        if (num1 <= 0 || num2 <= 0) {
+            System.out.println("num1とnum2は0より大きい値にしてください。");
+            return;
+        } else if (num1 >= num2) {
+            System.out.println("num2はnum1より大きい値にしてください。");
+            return;
+        } else if (num1 > 300 || num2 > 300) {
+            System.out.println("num1とnum2は300以下にしてください");
+            return;
+        }
+        for (int num = num1; num <= num2; num++) {
+            if (num % 3 == 0 && num % 5 == 0) {
                 System.out.println(num + "は3の倍数かつ5の倍数です。");
                 continue;
             } else if (num % 3 == 0) {
